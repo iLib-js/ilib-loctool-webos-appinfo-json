de568dfd519588b29fdb7f1133b5bfb406f3ee9d
Goun Lee <goun.lee@lge.com>
2019-11-08 18:17:46 +0900

    WIP. Add file for AppinfoJsonFile*

---
+++ AppinfoJsonFile.js
@@ -0,0 +1,203 @@
+/*
+ * AppinfoFile.js - plugin to extract resources from a appinfo.json code file
+ *
+ * Copyright © 2019, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var fs = require("fs");
+var path = require("path");
+var log4js = require("log4js");
+
+var logger = log4js.getLogger("loctool.plugin.AppinfoJsonFile");
+
+/**
+ * Create a new java file with the given path name and within
+ * the given project.
+ *
+ * @param {Project} project the project object
+ * @param {String} pathName path to the file relative to the root
+ * of the project file
+ * @param {FileType} type the file type of this instance
+ */
+var AppinfoJsonFile = function(props) {
+    this.project = props.project;
+    this.pathName = props.pathName;
+    this.type = props.type;
+    this.API = props.project.getAPI();
+
+    this.set = this.API.newTranslationSet(this.project ? this.project.sourceLocale : "zxx-XX");
+};
+
+/**
+ * Unescape the string to make the same string that would be
+ * in memory in the target programming language.
+ *
+ * @static
+ * @param {String} string the string to unescape
+ * @returns {String} the unescaped string
+ */
+AppinfoJsonFile.unescapeString = function(string) {
+    var unescaped = string;
+
+    unescaped = unescaped.
+        replace(/\\\\n/g, "").                // line continuation
+        replace(/\\\n/g, "").                // line continuation
+        replace(/^\\\\/, "\\").             // unescape backslashes
+        replace(/([^\\])\\\\/g, "$1\\").
+        replace(/^\\'/, "'").               // unescape quotes
+        replace(/([^\\])\\'/g, "$1'").
+        replace(/^\\"/, '"').
+        replace(/([^\\])\\"/g, '$1"');
+
+    return unescaped;
+};
+
+/**
+ * Clean the string to make a resource name string. This means
+ * removing leading and trailing white space, compressing
+ * whitespaces, and unescaping characters. This changes
+ * the string from what it looks like in the source
+ * code but increases matching.
+ *
+ * @static
+ * @param {String} string the string to clean
+ * @returns {String} the cleaned string
+ */
+AppinfoJsonFile.cleanString = function(string) {
+    var unescaped = AppinfoJsonFile.unescapeString(string);
+
+    unescaped = unescaped.
+        replace(/\\[btnfr]/g, " ").
+        replace(/[ \n\t\r\f]+/g, " ").
+        trim();
+
+    return unescaped;
+};
+
+/**
+ * Make a new key for the given string. This must correspond
+ * exactly with the code in htglob jar file so that the
+ * resources match up. See the class IResourceBundle in
+ * this project under the java directory for the corresponding
+ * code.
+ *
+ * @private
+ * @param {String} source the source string to make a resource
+ * key for
+ * @returns {String} a unique key for this string
+ */
+AppinfoJsonFile.prototype.makeKey = function(source) {
+    return AppinfoJsonFile.unescapeString(source);
+};
+
+/**
+ * Parse the data string looking for the localizable strings and add them to the
+ * project's translation set.
+ * @param {String} data the string to parse
+ */
+AppinfoJsonFile.prototype.parse = function(data) {
+    logger.debug("Extracting strings from " + this.pathName);
+    this.resourceIndex = 0;
+
+    var comment, match, key;
+
+    reGetString.lastIndex = 0; // just to be safe
+    var result = reGetString.exec(data);
+    while (result && result.length > 1 && result[3]) {
+        // different matches for single and double quotes
+        match = (result[3][0] === '"') ? result[4] : result[6];
+
+        if (match && match.length) {
+            logger.trace("Found string key: " + this.makeKey(match) + ", string: '" + match + "'");
+
+            var last = data.indexOf('\n', reGetString.lastIndex);
+            last = (last === -1) ? data.length : last;
+            var line = data.substring(reGetString.lastIndex, last);
+            var commentResult = reI18nComment.exec(line);
+            comment = (commentResult && commentResult.length > 1) ? commentResult[1] : undefined;
+
+            var r = this.API.newResource({
+                resType: "string",
+                project: this.project.getProjectId(),
+                key: AppinfoJsonFile.unescapeString(match),
+                sourceLocale: this.project.sourceLocale,
+                source: AppinfoJsonFile.cleanString(match),
+                autoKey: true,
+                pathName: this.pathName,
+                state: "new",
+                comment: comment,
+                datatype: this.type.datatype,
+                index: this.resourceIndex++
+            });
+            this.set.add(r);
+        } else {
+            logger.warn("Warning: Bogus empty string in get string call: ");
+            logger.warn("... " + data.substring(result.index, reGetString.lastIndex) + " ...");
+        }
+        result = reGetString.exec(data);
+    }
+ 
+    // now check for and report on errors in the source
+    this.API.utils.generateWarnings(data, reGetStringBogusConcatenation1,
+        "Warning: string concatenation is not allowed in the RB.getString() parameters:",
+        logger,
+        this.pathName);
+
+    this.API.utils.generateWarnings(data, reGetStringBogusConcatenation2,
+        "Warning: string concatenation is not allowed in the RB.getString() parameters:",
+        logger,
+        this.pathName);
+
+    this.API.utils.generateWarnings(data, reGetStringBogusParam,
+        "Warning: non-string arguments are not allowed in the RB.getString() parameters:",
+        logger,
+        this.pathName);
+};
+
+/**
+ * Extract all the localizable strings from the java file and add them to the
+ * project's translation set.
+ */
+AppinfoJsonFile.prototype.extract = function() {
+    logger.debug("Extracting strings from " + this.pathName);
+    if (this.pathName) {
+        var p = path.join(this.project.root, this.pathName);
+        try {
+            var data = fs.readFileSync(p, "utf8");
+            if (data) {
+                this.parse(data);
+            }
+        } catch (e) {
+            logger.warn("Could not read file: " + p);
+        }
+    }
+};
+
+/**
+ * Return the set of resources found in the current JavaScript file.
+ *
+ * @returns {TranslationSet} The set of resources found in the
+ * current JavaScript file.
+ */
+AppinfoJsonFile.prototype.getTranslationSet = function() {
+    return this.set;
+}
+
+// we don't localize or write javascript source files
+AppinfoJsonFile.prototype.localize = function() {};
+AppinfoJsonFile.prototype.write = function() {};
+
+module.exports = AppinfoJsonFile;
---
+++ AppinfoJsonFileType.js
@@ -0,0 +1,223 @@
+/*
+ * AppinfoFileType.js - Represents a collection of appinfo.json files
+ *
+ * Copyright © 2019, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var fs = require("fs");
+var path = require("path");
+var log4js = require("log4js");
+
+var AppinfoJsonFile = require("./AppinfoJsonFile.js");
+
+var logger = log4js.getLogger("loctool.plugin.AppinfoJsonFileType");
+
+var AppinfoJsonFileType = function(project) {
+    this.type = "json";
+    this.datatype = "json";
+    this.resourceType = "json";
+
+    this.project = project;
+    this.API = project.getAPI();
+
+    this.extensions = [".json"];
+
+    this.extracted = this.API.newTranslationSet(project.getSourceLocale());
+    this.newres = this.API.newTranslationSet(project.getSourceLocale());
+    this.pseudo = this.API.newTranslationSet(project.getSourceLocale());
+};
+
+/**
+ * Return true if the given path is a java file and is handled
+ * by the current file type.
+ *
+ * @param {String} pathName path to the file being questions
+ * @returns {boolean} true if the path is a java file, or false
+ * otherwise
+ */
+AppinfoJsonFileType.prototype.handles = function(pathName) {
+    logger.debug("AppinfoJsonFileTyp handles " + pathName + "?");
+    if (!pathName) return false;
+    
+    return (pathName === "appinfo.json") ? true : false
+    
+};
+
+AppinfoJsonFileType.prototype.name = function() {
+    return "Appinfo Json File Type";
+};
+
+/**
+ * Write out the aggregated resources for this file type. In
+ * some cases, the string are written out to a common resource
+ * file, and in other cases, to a type-specific resource file.
+ * In yet other cases, nothing is written out, as the each of
+ * the files themselves are localized individually, so there
+ * are no aggregated strings.
+ * @param {TranslationSet} translations the set of translations from the
+ * repository
+ * @param {Array.<String>} locales the list of locales to localize to
+ */
+AppinfoJsonFileType.prototype.write = function(translations, locales) {
+    // distribute all the resources to their resource files
+    // and then let them write themselves out
+
+    var resFileType = this.project.getResourceFileType(this.resourceType);
+
+    var res, file,
+        resources = this.extracted.getAll(),
+        db = this.project.db,
+        translationLocales = locales.filter(function(locale) {
+            return locale !== this.project.sourceLocale && locale !== this.project.pseudoLocale;
+        }.bind(this));
+
+    for (var i = 0; i < resources.length; i++) {
+        res = resources[i];
+
+        // for each extracted string, write out the translations of it
+        translationLocales.forEach(function(locale) {
+            logger.trace("Localizing JavaScript strings to " + locale);
+
+            db.getResourceByCleanHashKey(res.cleanHashKeyForTranslation(locale), function(err, translated) {
+                var r = translated;
+                if (!translated || ( this.API.utils.cleanString(res.getSource()) !== this.API.utils.cleanString(r.getSource()) &&
+                    this.API.utils.cleanString(res.getSource()) !== this.API.utils.cleanString(r.getKey()))) {
+                    if (r) {
+                        logger.trace("extracted   source: " + this.API.utils.cleanString(res.getSource()));
+                        logger.trace("translation source: " + this.API.utils.cleanString(r.getSource()));
+                    }
+                    var note = r && 'The source string has changed. Please update the translation to match if necessary. Previous source: "' + r.getSource() + '"';
+                    var newres = res.clone();
+                    newres.setTargetLocale(locale);
+                    newres.setTarget((r && r.getTarget()) || res.getSource());
+                    newres.setState("new");
+                    newres.setComment(note);
+
+                    this.newres.add(newres);
+
+                    logger.trace("No translation for " + res.reskey + " to " + locale);
+                } else {
+                    if (res.reskey != r.reskey) {
+                        // if reskeys don't match, we matched on cleaned string.
+                        //so we need to overwrite reskey of the translated resource to match
+                        r = r.clone();
+                        r.reskey = res.reskey;
+                    }
+
+                    file = resFileType.getResourceFile(locale);
+                    file.addResource(r);
+                    logger.trace("Added " + r.reskey + " to " + file.pathName);
+                }
+            }.bind(this));
+        }.bind(this));
+    }
+
+    resources = this.pseudo.getAll().filter(function(resource) {
+        return resource.datatype === this.datatype;
+    }.bind(this));
+
+    for (var i = 0; i < resources.length; i++) {
+        res = resources[i];
+        if (res.getTargetLocale() !== this.project.sourceLocale && res.getSource() !== res.getTarget()) {
+            file = resFileType.getResourceFile(res.getTargetLocale());
+            file.addResource(res);
+            logger.trace("Added " + res.reskey + " to " + file.pathName);
+        }
+    }
+};
+
+AppinfoJsonFileType.prototype.newFile = function(path) {
+    return new AppinfoJsonFile({
+        project: this.project,
+        pathName: path,
+        type: this
+    });
+};
+
+AppinfoJsonFileType.prototype.getDataType = function() {
+    return this.datatype;
+};
+
+AppinfoJsonFileType.prototype.getResourceTypes = function() {
+    return {};
+};
+
+/**
+ * Return the name of the node module that implements the resource file type, or
+ * the path to a javascript file that implements the resource filetype.
+ * @returns {Function|undefined} node module name or path, or undefined if this file type does not
+ * need resource files
+ */
+AppinfoJsonFileType.prototype.getResourceFileType = function() {
+    return AppinfoJsonFileType;
+};
+
+/**
+ * Return the translation set containing all of the extracted
+ * resources for all instances of this type of file. This includes
+ * all new strings and all existing strings. If it was extracted
+ * from a source file, it should be returned here.
+ *
+ * @returns {TranslationSet} the set containing all of the
+ * extracted resources
+ */
+AppinfoJsonFileType.prototype.getExtracted = function() {
+    return this.extracted;
+};
+
+/**
+ * Add the contents of the given translation set to the extracted resources
+ * for this file type.
+ *
+ * @param {TranslationSet} set set of resources to add to the current set
+ */
+AppinfoJsonFileType.prototype.addSet = function(set) {
+    this.extracted.addSet(set);
+};
+
+/**
+ * Return the translation set containing all of the new
+ * resources for all instances of this type of file.
+ *
+ * @returns {TranslationSet} the set containing all of the
+ * new resources
+ */
+AppinfoJsonFileType.prototype.getNew = function() {
+    return this.newres;
+};
+
+/**
+ * Return the translation set containing all of the pseudo
+ * localized resources for all instances of this type of file.
+ *
+ * @returns {TranslationSet} the set containing all of the
+ * pseudo localized resources
+ */
+AppinfoJsonFileType.prototype.getPseudo = function() {
+    return this.pseudo;
+};
+
+/**
+ * Return the list of file name extensions that this plugin can
+ * process.
+ *
+ * @returns {Array.<string>} the list of file name extensions
+ */
+AppinfoJsonFileType.prototype.getExtensions = function() {
+    return this.extensions;
+};
+
+module.exports = AppinfoJsonFileType;
